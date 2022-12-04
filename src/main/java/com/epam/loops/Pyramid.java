4b4e23637a269f82618df7cd4eb4a491d23d1bac
Sarvarbek <112868538+Mr-Sarvarbek@users.noreply.github.com>
2022-12-04 17:46:49 +0500

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,35 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
+        int i, j, k, n, a;
+
+        n = cathetusLength;
+        a = n;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = a; j > 1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (k = i; k != 0; k--)
+            {
+                System.out.print(k);
+
+            }
+            a--;
+
+            for (int l = 2; l <= i; l++)
+            {
+                System.out.print(l);
+
+            }
+            System.out.println();
+        }
 
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
