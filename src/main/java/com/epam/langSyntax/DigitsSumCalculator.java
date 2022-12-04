ea45276481cd136cb489eefee25af7a4c207085f
Sarvarbek <112868538+Mr-Sarvarbek@users.noreply.github.com>
2022-12-04 17:42:26 +0500

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,13 @@
 package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
-
     public void calculateSum(int number) {
+        int b,c,d,e,sum;
+        b=(number%1000)/100;  
+        c=number/1000; 
+        e=(number%100)/10; 
+        d=number%10; 
+        sum=b+c+d+e;
+        System.out.println(sum);
     }
-
 }
