c9883fea8f356d5c05b143ce6b74eda1aa9cb298
Sarvarbek <112868538+Mr-Sarvarbek@users.noreply.github.com>
2022-12-04 17:43:04 +0500

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,9 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
     public void revert(int number) {
-    }
+        
+        System.out.println((number / 100)+((number % 100) / 10)+((number % 10) / 100));
 
+    }
 }
